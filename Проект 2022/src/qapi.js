30db0c51db44caa1c2cbcdc4ae83ca34fa2f9c73
kkristinakkorn <89091819+kkristinakkorn@users.noreply.github.com>
2022-08-13 19:47:05 +0300

    :+1:

---
+++ Проект 2022/src/qapi.js
@@ -0,0 +1,75 @@
+export default {
+	"pages": {
+		"root": {
+			"name": "root",
+			"children": [
+				"62f74af849c9db0023484f86",
+				"62f74af849c9db0023484f8a",
+				"62f74af849c9db0023484f8e",
+				"62f74af849c9db0023484f92",
+				"62f74af849c9db0023484f9a"
+			],
+			"id": "root",
+			"pageUrl": "root"
+		},
+		"62f74af849c9db0023484f86": {
+			"id": "62f74af849c9db0023484f86",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"62f74af849c9db0023484f8a": {
+			"id": "62f74af849c9db0023484f8a",
+			"name": "index",
+			"pageUrl": "index",
+			"seo": {
+				"og:description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"title": "Home | Website Example",
+				"og:title": "Home | Website Example",
+				"og:image": "https://uploads.quarkly.io/5f44d0da669357001e60ed14/images/website-example-quarkly.png?v=2020-11-05T19:48:01.806Z",
+				"description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference."
+			}
+		},
+		"62f74af849c9db0023484f8e": {
+			"id": "62f74af849c9db0023484f8e",
+			"pageUrl": "about",
+			"name": "About",
+			"seo": {
+				"og:title": "About | Website Example",
+				"og:description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"og:image": "https://uploads.quarkly.io/5f44d0da669357001e60ed14/images/website-example-quarkly.png?v=2020-11-05T19:48:01.806Z",
+				"title": "About | Website Example"
+			}
+		},
+		"62f74af849c9db0023484f92": {
+			"id": "62f74af849c9db0023484f92",
+			"seo": {
+				"og:image": "https://uploads.quarkly.io/5f44d0da669357001e60ed14/images/website-example-quarkly.png?v=2020-11-05T19:48:01.806Z",
+				"title": "Work | Website Example",
+				"og:title": "Work | Website Example",
+				"description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"og:description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference."
+			},
+			"pageUrl": "work",
+			"name": "Work"
+		},
+		"62f74af849c9db0023484f9a": {
+			"id": "62f74af849c9db0023484f9a",
+			"pageUrl": "contact",
+			"name": "Contact",
+			"seo": {
+				"og:image": "https://uploads.quarkly.io/5f44d0da669357001e60ed14/images/website-example-quarkly.png?v=2020-11-05T19:48:01.806Z",
+				"title": "Contact | Website Example",
+				"og:title": "Contact | Website Example",
+				"description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"og:description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference."
+			}
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
